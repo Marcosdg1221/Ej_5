784a8af69954d871be9b45b5e65f24f7369ad0bd
mdominguez <mdominguez@106-31.info2>
2023-02-09 10:19:25 +0100

    <

---
+++ Tema_5/src/ejercicio1/Buscaminas.java
@@ -0,0 +1,11 @@
+package ejercicio1;
+
+import java.util.*; //Importamos el java.util que necesitamos
+
+public class Buscaminas {
+	public static int[] ponMinas(int[] tabla) { // Tiradados es estático
+		
+		
+		return tabla;
+	}
+}
---
+++ Tema_5/src/ejercicio1/Main.java
@@ -0,0 +1,18 @@
+package ejercicio1;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner all = new Scanner(System.in);
+		int tabla[] = new int[20];
+		int casillaCaida[]= new int[20];
+		int seleccionar;
+		tabla = Buscaminas.ponMinas(tabla);
+		System.out.println(Arrays.toString(tabla));
+		System.out.println("¿Qué casilla quieres pisar?");
+		seleccionar = all.nextInt();
+		casillaCaida=Buscaminas.casilla(tabla);
+	}
+}
